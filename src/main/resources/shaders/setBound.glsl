dab7f232160c1ef7ffd7e9e68088cfd86977b8c3
bowen951209 <bowen951209@gmail.com>
2025-04-28 18:06:19 +0800

    Fix set bound with corners.

--- src/main/resources/shaders/setBound.glsl
+++ src/main/resources/shaders/setBound.glsl
@@ -4,7 +4,6 @@ layout (local_size_x = 16) in;
 
 layout (rg32f, binding = 0) uniform image2D img;
 
-
 void main() {
     uint id = gl_GlobalInvocationID.x;
     ivec2 imgSize = imageSize(img);
@@ -12,29 +11,48 @@ void main() {
     
     ivec2 storePos;
     vec4 storeValue;
-    if (id < imgSize.x) {
+
+    // Corners
+    if(id == 0) {
+        // Top-left corner
+        storePos = ivec2(0, 0);
+        storeValue = 0.5 * (imageLoad(img, ivec2(1, 0)) + imageLoad(img , ivec2(0, 1)));
+    } else if (id == imgSize.x - 1) {
+        // Top-right corner
+        storePos = ivec2(imgSize.x - 1, 0);
+        storeValue = 0.5 * (imageLoad(img, ivec2(imgSize.x - 2, 0)) + imageLoad(img , ivec2(imgSize.x - 1, 1)));
+    } else if (id == imgSize.x * (imgSize.y - 1)) {
+        // Bottom-left corner
+        storePos = ivec2(0, imgSize.y - 1);
+        storeValue = 0.5 * (imageLoad(img, ivec2(1, imgSize.y - 1)) + imageLoad(img , ivec2(0, imgSize.y - 2)));
+    } else if (id == imgSize.x * imgSize.y - 1) {
+        // Bottom-right corner
+        storePos = ivec2(imgSize.x - 1, imgSize.y - 1);
+        storeValue = 0.5 * (imageLoad(img, ivec2(imgSize.x - 2, imgSize.y - 1)) + imageLoad(img , ivec2(imgSize.x - 1, imgSize.y - 2)));
+    }
+
+    // Edges
+    else if (id < imgSize.x) {
         // Top edge (left to right)
         storePos = ivec2(id, 0);
         storeValue = imageLoad(img, storePos + ivec2(0, 1));
         storeValue.y *= -1.0;
-        imageStore(img, storePos, storeValue);
     } else if (id < 2 * imgSize.x) {
         // Bottom edge (left to right)
         storePos = ivec2(id - imgSize.x, imgSize.y - 1);
         storeValue = imageLoad(img, storePos + ivec2(0, -1));
         storeValue.y *= -1.0;
-        imageStore(img, storePos, storeValue);
     } else if (id < 2 * imgSize.x + imgSize.y - 2) {
         // Left edge (top to bottom, excluding corners)
         storePos = ivec2(0, id - 2 * imgSize.x + 1);
         storeValue = imageLoad(img, storePos + ivec2(1, 0));
         storeValue.x *= -1.0;
-        imageStore(img, storePos, storeValue);
     } else {
         // Right edge (top to bottom, excluding corners)
         storePos = ivec2(imgSize.x - 1, id - (2 * imgSize.x + imgSize.y - 2) + 1);
         storeValue = imageLoad(img, storePos + ivec2(-1, 0));
         storeValue.x *= -1.0;
-        imageStore(img, storePos, storeValue);
     }
+
+    imageStore(img, storePos, storeValue);
 }
